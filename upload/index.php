616557fb8ad510ef42fc76e08d58a049bbc7c57a
leandrohenrique <leandrohenrique.costasilva@gmail.com>
2018-05-05 01:04:33 -0300

    upload

--- upload/index.php
+++ upload/index.php
@@ -1,4 +1,4 @@
-<form method="POST" enctype="multipart/form-data">
+<form method="POST" enctype="multipart/form-data" action="recebedor.php">
 	<input type="file" name="arquivo"><br/><br/>
 
 	<input type="submit" name="Enviar">
--- upload/recebedor.php
+++ upload/recebedor.php
@@ -1,3 +1,11 @@
 <?php
 
 $arquivo = $_FILES['arquivo'];
+
+if (isset($arquivo['tmp_name']) && !empty($arquivo['tmp_name'])) {
+
+	$nomearquivo = md5(time().rand(0,99)).'png';
+	move_uploaded_file($arquivo['tmp_name'], 'src/' . $arquivo['name']);
+
+	echo "Arquivo enviado com sucesso!";
+}
