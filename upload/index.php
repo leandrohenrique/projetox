93cc05fb74426eb5566e66d93995e504f67d0f56
leandrohenrique <leandrohenrique.costasilva@gmail.com>
2018-05-05 00:45:13 -0300

    upload

---
+++ upload/index.php
@@ -0,0 +1,6 @@
+<form method="POST" enctype="multipart/form-data">
+	<input type="file" name="arquivo"><br/><br/>
+
+	<input type="submit" name="Enviar">
+	
+</form>
---
+++ upload/recebedor.php
@@ -0,0 +1,3 @@
+<?php
+
+$arquivo = $_FILES['arquivo'];
