1cbb5773ada8dea472decec37b9966a20d2571fc
Leandro Henrique <leandrohenrique@unirg.edu.br>
2018-05-15 17:22:07 -0300

    Orientação a Objetos

---
+++ phpoo/index.php
@@ -0,0 +1,55 @@
+<?php
+class Pessoa 
+{
+	private $nome;
+	private $idade;
+
+	public function __construct($nome, $idade)
+	{
+		$this->$nome = $nome;
+		$this->$idade = $idade;
+	}
+
+	// public function get()
+	// {
+	// 	return "{$this->nome} - {$this->idade}";
+	// }
+
+	public function getNome()
+	{
+		return $this->nome;
+	}
+
+	public function setNome($nome)
+	{
+		if(is_string($nome)){
+			$this->nome = $nome;	
+		}
+	}
+
+	public function getIdade()
+	{
+		return $this->idade;
+	}
+
+	public function setIdade($idade)
+	{
+		$this->idade = $idade;
+	}
+}
+
+$l = new Pessoa("Leandro", "27");
+// $l->setNome("Leandro");
+// $l->setIdade(27);
+echo $l->getNome();die();
+echo "Nome: ".$l->getNome()."<br>";
+echo "Idade: ".$l->getIdade()."<br>";
+echo "<hr>";
+	
+// $f = new Pessoa();
+// $f->setNome("Fulano");
+// $f->setIdade(99);
+
+// echo "Nome: ".$f->getNome()."<br>";
+// echo "Idade: ".$f->getIdade()."<br>";
+// echo "<hr>";
