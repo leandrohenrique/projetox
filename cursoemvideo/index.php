09a519704c335238edf6bf4d02777a32d14ae47c
Leandro Henrique <leandrohenrique@unirg.edu.br>
2018-05-24 17:31:41 -0300

    Atualizando 1.2

--- cursoemvideo/index.php
+++ cursoemvideo/index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="UTF-8">
@@ -8,7 +8,7 @@
 
 		<?php
 
-			require_once 'Caneta.php';
+/*			require_once 'Caneta.php';
 			$caneta = new Caneta("BIC", "Azul");
 
 			//$caneta->setModelo("BIC");
@@ -19,7 +19,16 @@
 			echo "Modelo da caneta " . $caneta->getModelo()."<br>";
 			echo "Cor da caneta " .$caneta->getCor();
 
-			var_dump($caneta);
+			var_dump($caneta);*/
 		?>
 	</body>
-</html>
+</html> -->
+<?php
+echo "Data";
+// Descobrir Mês Anterior
+  echo date('d/m/Y', strtotime('-1 months', strtotime(date('Y-m-d'))));
+  // Resultado: 26/01/2014
+
+ // Descobrir Próximo Mês
+  echo date('d/m/Y', strtotime('+1 months', strtotime(date('Y-m-d'))));
+  // Resultado: 26/03/2014
