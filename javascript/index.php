c7d73d50863db879dd55b7a93271bdfd658395f7
Leandro Henrique <leandrohenrique@unirg.edu.br>
2018-07-13 12:53:18 -0300

    Estudando Java Script cap4

--- javascript/index.php
+++ javascript/index.php
@@ -1,25 +1,28 @@
 <html>
 	<head>
 		<title>Java Script</title>
+		<script src='funcs.js' type='text/javascript'></script>
 	</head>	
+	<body>
 		<table>
 			<tbody>
-				<tr>
+				<tr class="produto"><!-- linha da tabela -->
 					<td>
-						<div>R$ 29,90</div>
+						<div class="price">R$ 29,90</div>
 					</td>
 					<td>
-						<input type="number" name="">
+						<input type="number" class="quantity">
 					</td>
 				</tr>
 			</tbody>
 			<tr>
 				<td></td>
 				<td>Total de compras</td>
-				<td><div>R$ 29,90</div></td>
+				<td><div id="total">R$ 29,90</div></td>
 				<td></td>
 			</tr>
 		</table>
+	</body>
 </html>
 
 
